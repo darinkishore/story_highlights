59e659f94d716ccbe8a6e6bb1fe32a9f4b37af8d
darin <darinkishore@protonmail.com>
2024-01-22 17:02:32 -0500

    feat(views): add template rendering
    Add template rendering using Django's `loader.get_template` and `HttpResponse(template.render)` functions. This allows for a more modular and maintainable approach to rendering HTML templates.
    
    feat(views): update highlight function
    
    Update the `highlight` function to handle HTMX requests. If the request is an HTMX request and the method is POST, the function should get the text from the input field, call the `highlight_text` function from `transformations/src/highlight_ai.py` to highlight the text, and return the HTML-highlighted text.

--- transformations/views.py
+++ transformations/views.py
@@ -3,33 +3,24 @@ from django.shortcuts import render
 from datetime import datetime
 from django.http import HttpResponse
 from .forms import TextProcessingForm
+from django.template import loader
 
 def index(request):
-    now = datetime.now()
-    html = f'''
-    <html>
-        <body>
-            <h1>Hello from Vercel!</h1>
-            <p>The current time is { now }.</p>
-        </body>
-    </html>
-    '''
-    return HttpResponse(html)
-
+    template = loader.get_template("transformations/index.html")
+    return HttpResponse(template.render({}, request))
+    
+    
 # TODO: change so we don't use form anymore
 
 def highlight(request):
-    if request.method == 'POST':
-        form = TextProcessingForm(request.POST)
-        if form.is_valid():
-            # Process form data
-            # ...
-            # Return an HTMX response (e.g., JsonResponse with the processed text)
+    if request.htmx:
+        if request.method == 'POST':
+            # get text from input field
+            # highlight the text by calling the highlight_text function
+            # from transformations/src/highlight_ai.py
+            # return the html-highlighted text
             raise NotImplementedError
-    else:
-        form = TextProcessingForm()
-
-    return render(request, 'highlight.html', {'form': form})
+            
 
 def edit_1(request):
     if request.method == 'POST':
