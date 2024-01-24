5da997e9f0ac1c6c11b1fe0cc7a0c97b13bad577
darin <darinkishore@protonmail.com>
2024-01-23 22:08:11 -0500

    Remove test_utils.py from transformations/dat/prompts directory
    This commit removes the `test_utils.py` file from the `transformations/dat/prompts` directory as it is no longer needed. The tests that it contained have presumably been integrated into other tests which correctly handle similar scenarios. The file has been deleted to avoid confusion and redundancy in the test suite.

--- transformations/dat/test_integration.py
+++ transformations/dat/test_integration.py
@@ -5,3 +5,4 @@ from transformations.dat.reference_stories.reference import reference_stories
 from transformations.dat.colors import get_color_mapping
 
 # This test should read in a story, process it with highlights, use it in the prompt templates, and verify that the prompt templates are created properly. Here's a basic example of how you might structure this test:
+
