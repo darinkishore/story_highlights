48d6b662d8f219d60b06810aa0265028b7be62a4
Darin <86675935+darinkishore@users.noreply.github.com>
2024-01-25 16:11:46 -0500

    nice

--- vercel_app/settings.py
+++ vercel_app/settings.py
@@ -47,7 +47,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "whitenoise.runserver_nostatic",
-    # "django_htmx",
+    "django_htmx",
 ]
 
 MIDDLEWARE = [
@@ -59,7 +59,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    # "django_htmx.middleware.HtmxMiddleware",
+    "django_htmx.middleware.HtmxMiddleware",
 ]
 
 
