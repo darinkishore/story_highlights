2a0db838596939418289b2dc1710010b00d2d49e
Darin <86675935+darinkishore@users.noreply.github.com>
2024-01-25 21:06:16 -0500

    nice

--- transformations/urls.py
+++ transformations/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path
-from .views import edit_1, edit_2, highlight, index
+from .views import highlight, index
 
 urlpatterns = [
     path('', index, name='index'),
-    path('edit_1', edit_1, name='edit_1'),
-    path('edit_2', edit_2, name='edit_2'),
     path('highlight', highlight, name='highlight'),
 ]
--- transformations/views.py
+++ transformations/views.py
@@ -3,9 +3,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 from .dat.models import StoryHighlights
-from .forms import TextProcessingForm
 from django.template import loader
 from transformations.src.highlight_ai import label_story
+from asgiref.sync import async_to_sync
+from django.views.decorators.csrf import csrf_exempt
 
 html_placeholder = "<p>Here is some text</p>"
 
@@ -15,6 +16,7 @@ def index(request):
     return HttpResponse(template.render({}, request))
 
 
+@csrf_exempt
 async def highlight(request):
     if request.method == "POST" and request.htmx:
         text = request.POST.get("input_text")
